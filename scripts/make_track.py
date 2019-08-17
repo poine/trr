cb141e0bbcfd104bff81b25df18665bd205f3520
poine <poinix@gmail.com>
2019-08-17 23:28:12 +0200

    importing things

---
+++ scripts/make_track.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys, os, logging, numpy as np, matplotlib.pyplot as plt
+import rospkg
+import two_d_guidance as tdg
+import two_d_guidance.trr.utils as trr_u
+import pdb
+
+def make_z_room_path(res=0.01):
+   #x0, x1 = -0.75, 2.25
+   x0, x1 = 0., 2.25  # moved start of path to center
+   c1, r = [x1,0], 1.25
+   line1 = tdg.make_line_path([x0, r], [x1, r], res=res)
+   circle1 = tdg.make_circle_path(c1, -r, np.pi/2, np.pi, res=res)
+   x2, y2 = 1.581, -0.35
+   line2 = tdg.make_line_path([x1, -r], [x2, -r], res=0.01)
+   c2, r2, th2 = [x2, y2], 0.9, np.deg2rad(61.8)
+   circle2 = tdg.make_circle_path(c2, r2, -np.pi/2, -th2, res=res)
+   c3, r3 = [0, -1.21], 0.9
+   circle3 = tdg.make_circle_path(c3, r3, np.pi/2-th2, 2*th2, res=res)
+   c4, r4 = [-x2, y2], 0.9
+   circle4 = tdg.make_circle_path(c4, r4, -(np.pi/2-th2), -th2, res=res)
+   line3 = tdg.make_line_path([-x2, -r], [-x1, -r], res=res)
+   circle5 = tdg.make_circle_path([-x1, 0], -r, -np.pi/2, np.pi, res=res)
+   line4 = tdg.make_line_path([-x1, r], [x0, r], res=res)
+   line1.append([circle1, line2, circle2, circle3, circle4, line3, circle5, line4])
+   p = line1
+   tdg_dir = rospkg.RosPack().get_path('two_d_guidance')
+   fname = os.path.join(tdg_dir, 'paths/demo_z/track_trr_real.npz')
+   p.save(fname)
+   return fname, p
+
+def filter(vel_sps):
+   ref = tdg.utils.SecOrdLinRef(omega=3, xi=0.9)
+   out = np.zeros(len(vel_sps))
+   dt = 0.01 #WTF...
+   phi = 50
+   vel_sps2 = np.zeros(len(vel_sps))
+   vel_sps2[:-phi] = vel_sps[phi:]
+   vel_sps2[-phi:] = vel_sps[-phi] # cst....
+   out[0] = vel_sps2[0]; ref.reset(np.array([out[0], 0, 0]))
+   for i in range(1,len(vel_sps)):
+      out[i] = ref.run(dt, vel_sps2[i])[0]
+   return out, vel_sps2
+
+def add_vel_profile(fname):
+   tdg_dir = rospkg.RosPack().get_path('two_d_guidance')
+   fname = os.path.join(tdg_dir, 'paths/demo_z/track_trr_real.npz')
+   path = trr_u.TrrPath(fname)
+   for i, c in enumerate(path.curvatures):
+      path.vels[i] = 1. - 0.25*abs(c) # why not ...
+   fltrd, foo = filter(path.vels)
+   if 1:
+      plt.plot(fltrd)
+      plt.plot(foo)
+      plt.plot(path.vels)
+      plt.show()
+   path.vels = fltrd
+   fname = os.path.join(tdg_dir, 'paths/demo_z/track_trr_real_vel.npz')
+   path.save(fname)
+   p2 = trr_u.TrrPath(fname)
+   return p2
+   
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    np.set_printoptions(linewidth=300, suppress=True)
+    fname, p = make_z_room_path()
+    p2 = add_vel_profile(fname)
+    #tdg.draw_path(plt.gcf(), plt.gca(), p)
+    #plt.figure()
+    tdg.draw_path_curvature(plt.gcf(), plt.gca(), p)
+    plt.figure()
+    tdg.draw_path_vel(plt.gcf(), plt.gca(), p2)
+    plt.show()
+
---
+++ scripts/race_manager_display.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os, sys
+import math, numpy as np
+import rospy, cv2
+
+import pdb
+
+import two_d_guidance.trr.rospy_utils as trr_rpu
+
+
+class ImgPublisher(trr_rpu.DebugImgPublisher):
+    def __init__(self, img_topic):
+        trr_rpu.DebugImgPublisher.__init__(self, img_topic, '/trr_race_manager/image_debug')
+
+    def _draw(self, img_bgr, model, data):
+        y0=20; font_color=(0,255,255)
+        f, h, c, w = cv2.FONT_HERSHEY_SIMPLEX, 1.25, font_color, 2
+        cv2.putText(img_bgr, 'Race Manager', (y0, 40), f, h, c, w)
+        _m, _cl, _tl = model.get()
+        cv2.putText(img_bgr, 'mode {}'.format(trr_rpu.RaceManagerStatusStr(_m)), (y0, 90), f, h, c, w)
+        cv2.putText(img_bgr, 'lap {}/{}'.format(_cl, _tl), (y0, 140), f, h, c, w)
+
+class Node(trr_rpu.PeriodicNode):
+    def __init__(self):
+        trr_rpu.PeriodicNode.__init__(self, 'race_manager_display_node')
+        robot_name = rospy.get_param('~robot_name', '')
+        def prefix(robot_name, what): return what if robot_name == '' else '{}/{}'.format(robot_name, what)
+        cam_name = rospy.get_param('~camera', prefix(robot_name, 'camera_road_front'))
+        self.im_pub = ImgPublisher(cam_name)
+        self.sta_sub = trr_rpu.RaceManagerStatusSubscriber(what='race manager display node')
+
+    def periodic(self):
+        self.im_pub.publish(self.sta_sub, None)
+    
+def main(args): Node().run(10)
+
+if __name__ == '__main__':
+    main(sys.argv)
